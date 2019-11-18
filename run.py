2aa66c20d751dbe90e263d62d5ddeba92d03c1c2
Tclack88 <39845330+Tclack88@users.noreply.github.com>
2019-11-18 15:33:34 -0800

    Update run.py
    added cleaner_func to main

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import psycopg2
 
 from salt.retriever.log import startLog, getLogFile
 from salt.retriever.dbfuncs import create_tables, get_max_id_retrieved
-from salt.retriever.apifuncs import get_max_item
+from salt.retriever.apifuncs import get_max_item, cleaner_func
 from salt.retriever.retriever import retrieve_and_add_item
 from salt.models.ScoringFunctions import score_func
 
@@ -26,7 +26,7 @@ def main():
 		stop = start + 50
 
 		for i in range(start, stop):
-			retrieve_and_add_item(psql_conn, i, score_func=score_func)
+			retrieve_and_add_item(psql_conn, i, score_func=score_func, cleaner_func=cleaner_func)
 
 
 if __name__ == '__main__':
